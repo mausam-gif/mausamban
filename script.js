4c5c328bf4ca3c4755df469dbdca9d396f010464
mausam-gif <61419144+mausam-gif@users.noreply.github.com>
2023-11-25 23:24:31 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -77,13 +77,14 @@ welcomeScreen.to(".loading-screen", {
   y: -2000,
   ease: "Power4.out",
 });
+// to make the document floating animation timing
 welcomeScreen.from(
   ".home",
   {
-    y: 200,
-    duration: 5,
+    y: 100,
+    duration: 1,
     stagger: {
-      amount: 5,
+      amount: 1,
     },
   },
   "-=.8"
@@ -91,7 +92,7 @@ welcomeScreen.from(
 
 }
 loader()
-
+// now for audio
 function audio()
 {
 document.addEventListener("DOMContentLoaded", function () {
@@ -129,3 +130,92 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 }
 audio()
+
+//circles
+
+function circle() {
+  const circles = document.querySelectorAll('.circle');
+  circles.forEach(elem => {
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent = Math.floor((dots * marked) / 100);
+    var points = "";
+    var rotate = 360 / dots;
+
+    for (let i = 0; i < dots; i++) {
+      points += ` <div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    }
+    elem.innerHTML = points;
+
+    const pointsMarked = elem.querySelectorAll('.points');
+    for (let i = 0; i < percent; i++) {
+      pointsMarked[i].classList.add('marked');
+    }
+  });
+
+
+// Call the circle function initially
+
+
+// Set an interval to call the circle function every 2 seconds
+const intervalId = setInterval(circle, 9000);
+}
+circle()
+
+//for menu
+function menu() {
+let  menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
+function activeMenu() {
+  let len = section.length;
+  while (--len && window.scrollY + 97 < section[len].offsetTop){}
+  menuLi.forEach(sec => sec.classList.remove("active"));
+  menuLi[len].classList.add("active");
+}
+activeMenu();
+window.addEventListener("scroll",activeMenu); 
+}
+menu()
+
+//stiky navbar
+
+const header = document.querySelector("header");
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",window.scrollY > 50);
+
+})
+
+//toogle
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menuIcon.onclick = ()=>{
+  menuIcon.classList.toggle("bx-x")
+  navlist.classList.toggle("open");
+}
+
+window.onscroll= ()=>{
+  menuIcon.classList.remove("bx-x")
+  navlist.classList.remove("open");
+}
+
+//parallax//
+
+const observer = new IntersectionObserver((entries)=>{
+  entries.forEach((entry)=>{
+    if(entry.isIntersecting){
+     entry.target.classList.add("show-items");
+    }else{
+      entry.target.classList.remove("show-items");
+    }
+  });
+});
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el)=>observer.observe(el));
+
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollTop.forEach((el)=>observer.observe(el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el)=>observer.observe(el));
